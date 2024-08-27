a15c303d26f0421759e7800b469efe3b39a8ef72
mohahu <mohan17122k@gmail.com>
2024-08-27 10:30:35 +0530

    push

--- app/src/main/java/com/example/appshortcuts/MainActivity.kt
+++ app/src/main/java/com/example/appshortcuts/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.example.appshortcuts
 
-import android.content.Intent
-import android.content.pm.ShortcutInfo
-import android.content.pm.ShortcutManager
-import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.appshortcuts.databinding.ActivityMainBinding
 
 @RequiresApi(Build.VERSION_CODES.N_MR1)
@@ -24,24 +17,10 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
-        val shortcutManager = getSystemService(ShortcutManager::class.java)
+        val shortCutManagerRedirection = ShortCutManagerRedirection(this)
+        shortCutManagerRedirection.startDynamicShortcut()
 
-        val shortcut = ShortcutInfo.Builder(this, "dynamic_shortcut_id")
-            .setShortLabel("Open Feature")
-            .setLongLabel("Open Specific Feature")
-            .setIcon(Icon.createWithResource(this, R.drawable.ic_launcher_foreground))
-            .setIntent(
-
-                Intent(this, MainActivity::class.java, ).apply {
-                    putExtra("shortcut_id", "dynamic_shortcut_id")
-                    action = Intent.ACTION_VIEW
-                }
-            )
-            .build()
-
-        shortcutManager?.dynamicShortcuts = listOf(shortcut)
-
-        val args = intent?.getStringExtra("shortcut_id")
+        val args = intent?.getStringExtra(HOME_SHORT_CUT)
 
         println("ActivityMainBinding >>> $args")
 
---
+++ app/src/main/java/com/example/appshortcuts/ShortCutManagerRedirection.kt
@@ -0,0 +1,36 @@
+package com.example.appshortcuts
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.N_MR1)
+class ShortCutManagerRedirection(
+    private val context: Context
+) {
+
+    private val shortcutManager = context.getSystemService(ShortcutManager::class.java)
+
+    fun startDynamicShortcut() {
+        val shortcut = ShortcutInfo.Builder(context, "dynamic_shortcut_id")
+            .setShortLabel("Open Feature")
+            .setLongLabel("Open Specific Feature")
+            .setIcon(Icon.createWithResource(context, R.drawable.ic_launcher_foreground))
+            .setIntent(
+
+                Intent(context, MainActivity::class.java).apply {
+                    putExtra(HOME_SHORT_CUT, HOME_SHORT_CUT)
+                    action = Intent.ACTION_VIEW
+                }
+            )
+            .build()
+
+        shortcutManager?.dynamicShortcuts = listOf(shortcut)
+    }
+}
+
+const val HOME_SHORT_CUT = "HOME_SHORT_CUT"
